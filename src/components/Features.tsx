09d0e04a16aa456c1d7b06c21fcb043408a9c425
Onyiriuka Justin <richardjustin500@gmail.com>
2025-01-13 19:14:18 +0100

    features section completed

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,16 +1,24 @@
+import { featuresInfo } from "./constants/FeaturesInfo";
+import FeaturesCard from "./ui/FeaturesCard";
 import SectionHeader from "./ui/SectionHeader";
 
 function Features() {
   return (
-    <section id="#features" className="__section-padding">
+    <section
+      id="#features"
+      className="__section-padding flex flex-col items-center"
+    >
       <SectionHeader
         title="Streamlined for easy management"
         description="Enjoy customizable lists and smart tracking all in one place. Set tasks, get reminders, and see your progress simply and quickly."
         tag="Everything you need"
       />
-      <main className="flex flex-col items-center">
-        
-      </main>
+
+      <div className="grid grid-cols-1 lg:grid-cols-2 my-8 gap-9">
+        {featuresInfo.map((feature) => (
+          <FeaturesCard feature={feature} />
+        ))}
+      </div>
     </section>
   );
 }
---
+++ src/components/ui/FeaturesCard.tsx
@@ -0,0 +1,18 @@
+interface FeaturesCardProp {
+  feature: Record<string, string>;
+}
+
+function FeaturesCard({ feature }: FeaturesCardProp) {
+  const { image, title, description } = feature;
+  return (
+    <main className="flex flex-col justify-center items-center max-w-[19rem] p-5 rounded-lg shadow-md">
+      <img src={image} alt="feature image" className="w-40" />
+      <h1 className="text-lg font-semibold __text-grad">{title}</h1>
+      <p className="text-center font-light text-[#010D3E] text-sm my-3">
+        {description}
+      </p>
+    </main>
+  );
+}
+
+export default FeaturesCard;
